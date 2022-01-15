63ffc516143df0c773c0eeaae158307dbefe1f75
kwonsean <gownn12@naver.com>
2022-01-15 15:58:06 +0900

    feat: 여러컴포넌트 통합
    - Sheet 부분에서 컴포넌트가 2개라 하나의 컴포넌트로 통합하여 라우터 연결

---
+++ src/Sheet_JS/SheetMain.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sheet from './Sheet'
+import SheetSetRange from './SheetSetRange'
+
+export default function SheetMain() {
+  return (
+    <>
+      <Sheet />
+      <SheetSetRange />
+    </>
+  )
+}
